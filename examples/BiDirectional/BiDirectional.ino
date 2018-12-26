954cca91eac43ea5df9a3361f96dd0ab820f66ab
Diego Dorado <diegodorado@gmail.com>
2018-12-26 18:19:00 -0300

    updates adafruit defines to match those given by the dashboard

--- examples/BiDirectional/BiDirectional.ino
+++ examples/BiDirectional/BiDirectional.ino
@@ -1,14 +1,14 @@
 #include "SoftwareSerial.h"
 #include "ObloqAdafruit.h"
 
-#define WIFI_SSID         "myWifiSSID"
-#define WIFI_PASSWORD     "myWIFI_PWD"
-#define ADAFRUIT_USERNAME "adafruit_user"
-#define ADAFRUIT_KEY      "adafruit_key"
+#define WIFI_SSID      "myWifiSSID"
+#define WIFI_PASSWORD  "myWIFI_PWD"
+#define IO_USERNAME    "adafruit_user"
+#define IO_KEY         "adafruit_key"
+
 
 SoftwareSerial softSerial(10,11);
-ObloqAdafruit olq(&softSerial,WIFI_SSID,WIFI_PASSWORD,ADAFRUIT_USERNAME,ADAFRUIT_KEY);
-
+ObloqAdafruit olq(&softSerial,WIFI_SSID,WIFI_PASSWORD,IO_USERNAME,IO_KEY);
 /*
 This example sketch will subscribe to a "please-log" topic
 And upon a message is received, will publish "40" to "ana-log" topic
--- examples/SimpleReceive/SimpleReceive.ino
+++ examples/SimpleReceive/SimpleReceive.ino
@@ -1,14 +1,14 @@
 #include "SoftwareSerial.h"
 #include "ObloqAdafruit.h"
 
-#define WIFI_SSID         "myWifiSSID"
-#define WIFI_PASSWORD     "myWIFI_PWD"
-#define ADAFRUIT_USERNAME "adafruit_user"
-#define ADAFRUIT_KEY      "adafruit_key"
+#define WIFI_SSID      "myWifiSSID"
+#define WIFI_PASSWORD  "myWIFI_PWD"
+#define IO_USERNAME    "adafruit_user"
+#define IO_KEY         "adafruit_key"
 
 
 SoftwareSerial softSerial(10,11);
-ObloqAdafruit olq(&softSerial,WIFI_SSID,WIFI_PASSWORD,ADAFRUIT_USERNAME,ADAFRUIT_KEY);
+ObloqAdafruit olq(&softSerial,WIFI_SSID,WIFI_PASSWORD,IO_USERNAME,IO_KEY);
 
 void msgHandle(const String& topic,const String& msg)
 {
